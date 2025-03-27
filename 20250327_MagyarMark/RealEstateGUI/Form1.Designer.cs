4b7fe01a9ad421cf23b7dca7d28065fd7360d877
MagyarMark <118881797+MagyarMark@users.noreply.github.com>
2025-03-27 17:08:32 +0100

    RealEstateGui update

--- 20250327_MagyarMark/RealEstateGUI/Form1.Designer.cs
+++ 20250327_MagyarMark/RealEstateGUI/Form1.Designer.cs
@@ -29,24 +29,101 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.hbe = new System.Windows.Forms.Button();
+            this.tel = new System.Windows.Forms.Label();
+            this.nev = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hSzam = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox1
             // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(168, 420);
+            this.listBox1.Size = new System.Drawing.Size(264, 53);
             this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65.43689F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34.56311F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 387F));
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.hbe, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tel, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.nev, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.hSzam, 2, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 290F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // hbe
+            // 
+            this.hbe.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.hbe.Location = new System.Drawing.Point(415, 121);
+            this.hbe.Name = "hbe";
+            this.hbe.Size = new System.Drawing.Size(382, 36);
+            this.hbe.TabIndex = 6;
+            this.hbe.Text = "Hírdetések betöltése";
+            this.hbe.UseVisualStyleBackColor = true;
+            this.hbe.Click += new System.EventHandler(this.hbe_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(273, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Eladó neve:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(273, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Eladó telefonszáma:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(273, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hírdetések száma:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Ingatlanok";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -54,6 +131,14 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label hSzam;
+        private System.Windows.Forms.Label tel;
+        private System.Windows.Forms.Label nev;
+        private System.Windows.Forms.Button hbe;
     }
 }
 
--- 20250327_MagyarMark/RealEstateGUI/Form1.cs
+++ 20250327_MagyarMark/RealEstateGUI/Form1.cs
@@ -2,19 +2,89 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace RealEstateGUI
 {
     public partial class Form1 : Form
     {
+        private Dictionary<string, (string phone, int number)> adatok = new Dictionary<string, (string, int)>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hbe.Dock = DockStyle.Fill;
+            listBox1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.SetRowSpan(listBox1, 4);
+            nev.Text = "";
+            tel.Text = "";
+            hSzam.Text = "";
+
+            string filePath = "nevek.txt";
+
+            if (File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(',');
+
+                    if (parts.Length == 3)
+                    {
+                        string name = parts[0].Trim();
+                        string phone = parts[1].Trim();
+                        int number = int.Parse(parts[2].Trim());
+
+                        adatok[name] = (phone, number);
+
+                        Debug.WriteLine($"Név: {name}, Telefonszám: {phone}, Véletlen szám: {number}");
+                        listBox1.Items.Add(name);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("HALO NINCS FILE");
+            }
+        }
+
+        private void hbe_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string valasztott = listBox1.SelectedItem.ToString();
+
+                if (adatok.ContainsKey(valasztott))
+                { 
+                    hSzam.Text = adatok[valasztott].number.ToString();
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string valasztott = listBox1.SelectedItem.ToString();
+
+                if (adatok.ContainsKey(valasztott))
+                {
+                    nev.Text = valasztott;
+                    tel.Text = adatok[valasztott].phone;
+                }
+            }
         }
     }
 }
