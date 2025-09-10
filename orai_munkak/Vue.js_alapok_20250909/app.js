131a4f300e2d34d8fc18d24f293d9f26913b7308
Magyar Márk <118881797+MagyarMark@users.noreply.github.com>
2025-09-10 09:30:56 +0200

    Vue.js Framework update

--- orai_munkak/Vue.js_alapok_20250909/app.js
+++ orai_munkak/Vue.js_alapok_20250909/app.js
@@ -2,15 +2,38 @@ new Vue({
     el: '#app',
     data: {
         hello: 'Hello World!',
+        reverseHello: '',
         tooltip: 'This is a tooltip text',
         color: 'redText',
         fontweight: 'boldText',
         StyleObject: {
             color: 'green',
             fontSize: '20px',
-        }
+        },
+        myheader: '<h2 style="color:blue">Rendered with v-html directive</h2>'
     },
     created: function() {
-        console.log('Vue instance created.');
+        this.reverseHello = this.hello.split(' ').reverse().join(' ');
+    },
+    methods: {
+        /*reverseHello() {
+            return this.hello.split(' ').reverse().join(' ');
+        },*/
+        capitalizeHello() {
+            return this.hello.toUpperCase();
+        },
+        add(a, b) {
+            return a + b;
+        }
+    },
+    /*watch: {
+        hello: function(newValue) {
+            this.reverseHello = newValue.split(' ').reverse().join(' ');
+        }
+    }*/
+    computed: {
+        reverseHello() {
+            return this.hello.split(' ').reverse().join(' ');
+        }
     }
 })
