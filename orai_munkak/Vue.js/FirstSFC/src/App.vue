3e7608ab57ff43bc3c9dcd2b4c97e8ac6117c121
Magyar Márk <118881797+MagyarMark@users.noreply.github.com>
2025-10-15 09:43:50 +0200

    Vue_3

---
+++ orai_munkak/Vue.js/FirstSFC/src/App.vue
@@ -0,0 +1,41 @@
+<script>
+/*export default {
+    data() {
+        return {
+            message: "Hello Vue 3"
+        }
+    }
+}*/
+</script>
+
+<template>
+    <h1>Ételek</h1>
+    <div id="wrapper">
+        <food-item 
+        food-name="Almák" 
+        food-desc="A különböző almák fákon teremnek" :is-favorite="true"/>
+        <food-item 
+        food-name="Pizza" 
+        food-desc="A pizza kenyértéstra feltétekkel" :is-favorite="false"/>
+        <food-item 
+        food-name="Rizs" 
+        food-desc="Nagyon sok ember fogyaszt rizst" :is-favorite="false"/>
+    </div>
+</template>
+
+<style>
+    #wrapper{
+        display: flex;
+        flex-wrap: wrap;
+    }
+    #wrapper > div{
+        border: dashed 1px black;
+        flex-basis: 120px;
+        margin: 10px;
+        padding: 10px;
+        background-color: lightgreen;
+    }
+    /*#app > div:hover{
+        cursor: pointer;
+    }*/
+</style>
---
+++ orai_munkak/Vue.js/FirstSFC/src/components/FoodItem.vue
@@ -0,0 +1,75 @@
+<template>
+    <div><!--@click="countClicks"-->
+        <h2>
+            {{ foodName }}
+            <img src="../img_quality.svg" v-show="foodisfavorite">
+        </h2>
+        <p>{{ foodDesc }}</p>
+        <button @click="togglefavorite">{{ foodisfavorite ? "Kedvenc törlése" : "Kedvenc hozzáadása" }}</button>
+        <!--<h2>{{ name }}</h2>
+        <p>{{ message }}</p>
+        <p id="red">Klikkelések száma: {{ clicks }}</p>-->
+    </div>
+</template>
+
+<script>
+export default {
+    //props:["foodName", "foodDesc", "isFavorite"],
+    props: {
+        foodName: {
+            type: String,
+            required: true // ha nincs megadva név akkor hiba üzenetet ad
+        },
+        foodDesc: {
+            type: String,
+            required: false,
+            default: "Ennek az ételnek nincs leírása",
+            validator: function (value) {
+                if (20 < value.length && value.length < 50) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        isFavorite: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+},    data() {
+        return {
+            foodisfavorite: this.isFavorite
+    }
+},
+    methods: {
+        togglefavorite() {
+            this.foodisfavorite = !this.foodisfavorite;
+    }
+},
+
+/*data() {
+        return {
+            name: "Alma",
+            message: "Szeretem az almát",
+            clicks: 0
+        }
+    },
+    methods: {
+        countClicks() {
+            this.clicks++
+        }
+    }*/
+}
+</script>
+
+<style>
+    /*#red{
+        font-weight: bold;
+        color: rgb(144, 12, 12);
+    }*/
+    img{
+        height: 1.5em;
+        float: right;
+    }
+</style>
